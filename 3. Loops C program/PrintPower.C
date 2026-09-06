c66798decf6a4065007e4b5c2b65e931770a948f
rajnish gupta <guptarajnishkumar89@gmail.com>
2026-09-06 13:49:34 +0530

    Add calculate power program

---
+++ 3. Loops C program/PrintPower.C
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int a,b;
+    printf ("enter the base:");
+     scanf("%d",&a);
+
+    printf("enter the power:");
+    scanf("%d",&b);
+
+    int power =1;
+    for(int i=1; i<=b; i++){
+        power = power *a;
+
+    }
+    printf("%d is raised to the power %d is %d",a,b ,power );
+    return 0;
+}
