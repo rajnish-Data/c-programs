285614c12c6327b045129a5a7b83b11d40cd5cc3
rajnish gupta <guptarajnishkumar89@gmail.com>
2026-09-06 14:42:29 +0530

    Add Simple Interest input program

---
+++ 1. C Program basic/SimpleIntrestINPUT.C
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    float p,r,t,si;
+    scanf("%f",&p); 
+     scanf("%f",&r); 
+      scanf("%f",&t);
+    si =(p*r*t)/100;
+    printf("%f",si);
+    return 0;
+}
