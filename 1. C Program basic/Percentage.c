de216654dbeede5688ecb33ee18bc530558e58bb
rajnish gupta <guptarajnishkumar89@gmail.com>
2026-09-06 13:28:10 +0530

    Add Percentage calculation program

---
+++ 1. C Program basic/Percentage.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    float m1 = 67; //maths marks
+    float m2 = 78; //biology marks
+    float m3 = 89; //english marks
+    float m4 = 90; //chemistry marks
+    float m5 = 56; //phyysics marks
+    float p=(m1+m2+m3+m4+m5)/500*100;
+    printf("%f",p);
+    return 0;
+}
