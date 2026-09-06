7e1bdcaa3cfc77160e428c8ad3d09067c4d25a03
rajnish gupta <guptarajnishkumar89@gmail.com>
2026-09-06 14:19:22 +0530

    sum of two numbers using functions

---
+++ 4. Function C program/TwonumSum.c
@@ -0,0 +1,10 @@
+ #include<stdio.h>
+ int sum(int a, int b){
+    return a+b;
+ }
+
+    int main(){
+        int result = sum(5, 10);
+        printf("The sum of numbers  is: %d\n", result);
+        return 0;
+    }
