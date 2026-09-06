e9d2a16d72c5c24dd7e5fe57a96cf9ca6025bd7e
rajnish gupta <guptarajnishkumar89@gmail.com>
2026-09-06 14:19:21 +0530

    Add basic function example

---
+++ 4. Function C program/BasicFunction.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void greet(){
+    printf("Hello, World!\n");
+    printf("good morning\n");
+    return;
+}
+int main(){
+    greet();
+    greet();
+    greet();
+    greet();
+    return 0;
+}
